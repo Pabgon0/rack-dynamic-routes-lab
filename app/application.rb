ac4144c88d21879b701d43f37c4c9e89abd60e71
Pablo Gonzalez <pabgon0@gmail.com>
2019-10-17 17:54:56 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      
+      item = req.path.split(/items/).last
+      
+    
+    
+    resp.finish
+  end
+end
